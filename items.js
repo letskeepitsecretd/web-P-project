765b66b87aca338682e7200989cca171688d97e5
DEV RAIKWAR <snatchcart18@gmail.com>
2026-04-14 16:17:33 +0530

    Create items.js

---
+++ items.js
@@ -0,0 +1,13 @@
+class Items {
+    constructor(id, eventName, organizer, eventDate, mode, teamSize, prizePool, email, phone) {
+        this.id = id;
+        this.eventName = eventName;
+        this.organizer = organizer;
+        this.eventDate = eventDate;
+        this.mode = mode;
+        this.teamSize = teamSize;
+        this.prizePool = prizePool;
+        this.email = email;
+        this.phone = phone;
+    }
+}
